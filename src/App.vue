ba87012ec1606a629b86631b5907e12d967dc952
David Mellen <d.mellen@remind.de>
2023-05-17 12:46:35 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+const color = '255 0 0'
+</script>
+
+<template>
+  <div class="headline">Headline</div>
+</template>
+
+<style lang="scss">
+.headline {
+  // works:
+  // --color: v-bind(color);
+  // color: rgb(var(--color));
+
+  // doesn't work
+  color: rgb(v-bind(color));
+}
+</style>
